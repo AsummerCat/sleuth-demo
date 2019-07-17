11e4772dacf824d4f72f14df2dd8e0a38aa1bec8
chenxiaocehng <583188551@qq.com>
2019-07-17 15:41:00 +0800

    Initial commit

---
+++ server-a-demo/src/main/java/com/linjingc/serverademo/ServerADemoApplication.java
@@ -0,0 +1,15 @@
+package com.linjingc.serverademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServerADemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerADemoApplication.class, args);
+    }
+
+}
---
+++ server-b-demo/src/main/java/com/linjingc/serverbdemo/ServerBDemoApplication.java
@@ -0,0 +1,15 @@
+package com.linjingc.serverbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServerBDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerBDemoApplication.class, args);
+    }
+
+}
